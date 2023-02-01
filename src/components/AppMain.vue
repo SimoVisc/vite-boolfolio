ecea548ef1dc910abe0e2bc4f3909f2a971fbd33
SimoVisc <simonaviscardivs@gmail.com>
2023-02-01 16:17:16 +0100

    Milestone 4: Stampa in console i dati del progetto Laravel

---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<script>
+import ListProjects from "./ListProjects.vue";
+export default {
+  name: "AppMain",
+  components: { ListProjects },
+};
+</script>
+
+<template>
+  <main>
+    <ListProjects />
+  </main>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/ListProjects.vue
@@ -0,0 +1,22 @@
+<script>
+import axios from "axios";
+export default {
+  name: "ListProjects",
+  created() {
+    axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
+      this.projects = response.data;
+    });
+  },
+};
+</script>
+
+<template>
+  <h2>List Projects</h2>
+  <ul>
+    <li v-for="project in projects">
+      {{ project.name }}
+    </li>
+  </ul>
+</template>
+
+<style lang="scss" scoped></style>
